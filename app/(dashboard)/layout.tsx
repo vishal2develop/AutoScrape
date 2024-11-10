24e43fbd38d10aa2c3351cfdc116941dfdbd43e3
Vishal Borana <vishal2develop@gmail.com>
2024-11-10 23:51:01 +0530

    Clerk Auth Implemented

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,6 +2,7 @@ import BreadCrumbHeader from "@/components/BreadCrumbHeader";
 import DesktopSidebar, { MobileSidebar } from "@/components/Sidebar";
 import { ModeToggle } from "@/components/ThemeModeToggle";
 import { Separator } from "@/components/ui/separator";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
@@ -14,6 +15,9 @@ const layout = ({ children }: { children: React.ReactNode }) => {
           <BreadCrumbHeader />
           <div className="gap-1 flex items-center">
             <ModeToggle />
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
           </div>
         </header>
         <Separator />
